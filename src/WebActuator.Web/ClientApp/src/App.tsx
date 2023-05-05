dc55f3bcd2bae74220b3f380fe994ab203d33451
token <239573049@qq.com>
2023-05-05 20:57:10 +0800

    优化界面

--- src/WebActuator.Web/ClientApp/src/App.tsx
+++ src/WebActuator.Web/ClientApp/src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css';
 
 import React, { Component } from 'react'
-import { Button, Layout, Nav } from '@douyinfe/semi-ui';
+import { Button, Divider, Layout, Nav, Tree } from '@douyinfe/semi-ui';
 import MonacoEditor from 'react-monaco-editor';
+import {
+  IconFolderOpen,
+} from "@douyinfe/semi-icons";
 
-let first = true;
+const { Header, Footer, Sider, Content } = Layout;
 
-const { Header, Footer, Content } = Layout;
-
+const treeData = [
+  {
+    label: 'Dotnet',
+    value: 'Asia',
+    key: '0',
+    children: [
+      {
+        label: 'Program.cs',
+        value: 'China',
+        key: '0-0',
+      },
+    ],
+  }
+];
 export default class App extends Component {
 
   state = {
@@ -40,22 +55,33 @@ export default class App extends Component {
       selectOnLineNumbers: true,
       automaticLayout: true,
     };
+    const style = {
+      width: '150px',
+      height: "calc(100% - 120px)",
+      border: '1px solid var(--semi-color-border)'
+    };
 
     return (
-      <div style={{ height: '100%' }}>
+      <Layout style={{ height: '100%' }}>
+
+        <Sider style={{ backgroundColor: "var(--semi-color-bg-1)", width: '150px' }} >
+          <div style={{
+            justifyContent: "space-between",
+            padding: "10px",
+            textAlign: "center",
+            color: "var(--semi-color-text-2)",
+          }}>
+            Web IDE
+          </div>
+          <Tree
+            treeData={treeData}
+            defaultExpandAll
+            directory
+            style={style}
+          />
+        </Sider>
         <Layout
           className="web-layout">
-          <Header style={{ backgroundColor: "var(--semi-color-bg-1)" }}>
-            <Nav
-              mode="horizontal">
-              <Nav.Header>
-                <span >C# Web IDE</span>
-              </Nav.Header>
-              <Nav.Item>
-                <span >运行</span>
-              </Nav.Item>
-            </Nav>
-          </Header>
           <Content style={{
             height: 'max-content',
           }}>
@@ -71,13 +97,13 @@ export default class App extends Component {
           </Content>
           <Footer
             style={{
-              display: "flex",
               justifyContent: "space-between",
               padding: "20px",
+              textAlign: "center",
               color: "var(--semi-color-text-2)",
             }}>.NET 7 Web Assembly</Footer>
         </Layout>
-      </div>
+      </Layout>
     )
   }
 }
