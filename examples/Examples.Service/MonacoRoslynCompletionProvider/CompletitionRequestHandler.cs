d5d52c6925d2b2490a14256e8c40dde71310e545
token <239573049@qq.com>
2023-08-04 13:23:32 +0800

    修改版本

--- examples/Examples.Service/MonacoRoslynCompletionProvider/CompletitionRequestHandler.cs
+++ examples/Examples.Service/MonacoRoslynCompletionProvider/CompletitionRequestHandler.cs
@@ -4,28 +4,28 @@ namespace Examples.Service.MonacoRoslynCompletionProvider
 {
     public static class CompletitionRequestHandler
     {
-        public async static Task<TabCompletionResult[]> Handle(TabCompletionRequest tabCompletionRequest)
+        public static async Task<TabCompletionResult[]> Handle(TabCompletionRequest tabCompletionRequest)
         {
             var workspace = CompletionWorkspace.Create(tabCompletionRequest.Assemblies);
             var document = await workspace.CreateDocument(tabCompletionRequest.Code);
             return await document.GetTabCompletion(tabCompletionRequest.Position, CancellationToken.None);
         }
 
-        public async static Task<HoverInfoResult> Handle(HoverInfoRequest hoverInfoRequest)
+        public static async Task<HoverInfoResult> Handle(HoverInfoRequest hoverInfoRequest)
         {
             var workspace = CompletionWorkspace.Create(hoverInfoRequest.Assemblies);
             var document = await workspace.CreateDocument(hoverInfoRequest.Code);
             return await document.GetHoverInformation(hoverInfoRequest.Position, CancellationToken.None);
         }
 
-        public async static Task<CodeCheckResult[]> Handle(CodeCheckRequest codeCheckRequest)
+        public static async Task<CodeCheckResult[]> Handle(CodeCheckRequest codeCheckRequest)
         {
             var workspace = CompletionWorkspace.Create(codeCheckRequest.Assemblies);
             var document = await workspace.CreateDocument(codeCheckRequest.Code);
             return await document.GetCodeCheckResults(CancellationToken.None);
         }
 
-        public async static Task<SignatureHelpResult> Handle(SignatureHelpRequest signatureHelpRequest)
+        public static async Task<SignatureHelpResult> Handle(SignatureHelpRequest signatureHelpRequest)
         {
             var workspace = CompletionWorkspace.Create(signatureHelpRequest.Assemblies);
             var document = await workspace.CreateDocument(signatureHelpRequest.Code);
--- src/WebActuator/ReferenceManage.cs
+++ src/WebActuator/ReferenceManage.cs
@@ -2,19 +2,29 @@
 using System.Collections.Concurrent;
 
 namespace WebActuator;
-public class ReferenceManage
+
+public static class ReferenceManage
 {
     private static readonly ConcurrentDictionary<string, MetadataReference> _references = new();
 
-    public static MetadataReference[] References => _references.Select(x => x.Value).ToArray();
+    public static IEnumerable<MetadataReference> References => _references.Select(x => x.Value).ToArray();
 
     public static string[] ReferenceKeys => _references.Select(x => x.Key).ToArray();
 
+    /// <summary>
+    /// 新增引用
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="reference"></param>
     public static void AddReference(string name, MetadataReference reference)
     {
         _references.TryAdd(name, reference);
     }
 
+    /// <summary>
+    /// 卸载引用
+    /// </summary>
+    /// <param name="name"></param>
     public static void RemoveReference(string name)
     {
         _references.TryRemove(name, out _);
